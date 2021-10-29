50951a2724d60ac61bf0ac6c345bed01af0f7ca3
s12001970 <90337159+s12001970@users.noreply.github.com>
2021-10-29 13:21:48 -0400

    0.2

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -6,3 +6,10 @@
 my_int = -15 # INTEGERS, any +/- whole number and 0
 my_float = 1.234567 # Floating point any +/- number with a decimal.
 my_string = "Where the food." 
+
+print(my_int)
+print(my_float)
+print(my_string)
+
+my_int = input ("Please type the number of hotdogs you can eat in 5 minutes")
+print (my_int)
