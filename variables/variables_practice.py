460e6d102a2a6cc32afaf39d7911413d081dae6e
s12001970 <90337159+s12001970@users.noreply.github.com>
2021-10-29 13:02:39 -0400

    0.1v
    added float intergers and strings

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables practice Durance McCool 10/29/21 12:48, version 0.0
+# Variables practice Durance McCool 10/29/21 1:01, version 0.1
+
+
+# Basic Varibles
+
+my_int = -15 # INTEGERS, any +/- whole number and 0
+my_float = 1.234567 # Floating point any +/- number with a decimal.
+my_string = "Where the food." 
